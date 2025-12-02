3387557b0207b60e706834f032b32ce974f582d6
Michael J.H. Taggart <mtaggarthawk@gmail.com>
2025-12-01 23:36:45 -0700

    final_project outline written up. starting it up!

---
+++ week06/final_project.py
@@ -0,0 +1,38 @@
+
+# ? What do we want the users to experience?
+
+# * User's will be able to create many projects each project can hold many tasks, and each task can hold many sessions.
+# * Each session will have a start and stop time.
+
+# * User should have some sort of way to create a "project".
+# * User should be able to see the projects they have created.
+# * User should have some sort of way to select a project.
+
+# * When a User has selected a project, they:
+
+# * User should have some sort of way to create a "task" they want to create sessions for.
+# * User should be able to see the tasks they have created.
+# * User should have some sort of way to select a "task" that they want to record more sessions for.
+
+# * When a User has selected a task, they:
+
+# * User should see a button to start/stop.
+
+# * When a User hits start, a visual counter stopwatch display is shown for "Time elapsed".
+# * Once Start is selected, there is only a Stop button. (no pause button).
+# * Each time the session is stopped, that is the end of the session.
+# * The timestamp of when the session started, and the timestamp of when the session ended is all that we need to store for each session. Sessions do not need names.
+
+# * The user should be able to also see all of the sessions recorded for a task.
+# * The user should see a total number of hours & minutes for their selected task.
+# * The user should see when their selected task was started/finished based on the recorded sessions.
+# * The user should see a total number of hours & minutes for their selected project.
+# * The user should see when their selected project was started/finished based on the tasks.
+
+# * EXTRA
+# * User's can select a session and edit the start date & time, edit their end date & time, and save their changes
+# * User's must select their timezone.
+
+# ? How should the design of this GUI look?
+
+# * I think it should take heavy inspiration with how the MacOs Finder application looks when choosing to show the items in the "Column" format
---
+++ week06/tutorial.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Simple App")
+
+
+def on_click():
+    lbl.config(text="Button Clicked")
+
+
+lbl = tk.Label(root, text="Label 1")
+lbl.grid(row=0, column=0)
+
+btn = tk.Button(root, text="Button 1", command=on_click)
+btn.grid(row=0, column=1)
+
+root.mainloop()
